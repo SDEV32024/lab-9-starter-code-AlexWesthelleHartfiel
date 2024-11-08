92271dd78f02226d418ad39d85fc62faf04b12c6
Alex Westhelle-Hartfiel <X00216043@mytudublin.ie>
2024-11-08 19:22:34 +0000

    Lab 9 part 2 commit

--- books/views.py
+++ books/views.py
@@ -6,8 +6,8 @@ from .models import Book
 class BookListView(ListView):
     model = Book
     contect_object_name = 'book_list'
-    template_name = 'books.book_list.html'
+    template_name = 'book_list.html'
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'books/book_detail.html'
+    template_name = 'book_detail.html'
